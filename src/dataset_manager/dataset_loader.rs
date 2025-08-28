a77b14c559b8eead21058857ed1e5c333dd0ad2d
VinoAsAuksInAMoat <83805270+VinoAsAuksInAMoat@users.noreply.github.com>
2025-08-29 04:33:14 +0900

    refactor: impl of fvecs loader to get file sizes faster and for the extensibility to load other data formats

--- src/dataset_manager/dataset_loader.rs
+++ src/dataset_manager/dataset_loader.rs
@@ -1,42 +1,94 @@
-use std::fs::File;
-use std::io::{Read, BufReader};
-use std::{time, rc::Rc};
-use std::collections::HashMap;
+use std::{
+    fs::{File, metadata}, 
+    fs, 
+    io::{Read, BufReader}, 
+    io, 
+    time, 
+    time::Duration, 
+    rc::Rc, 
+    collections::HashMap, 
+    cmp, 
+};
 
 use crate::common::data::*;
 
-pub fn load_fvecs(filename: &str, data_num: u64) -> Dataset {
-    println!("[Info] Load file: {}", filename);
-
-    let timer = time::Instant::now();
-
-    let mut reader = BufReader::new(File::open(filename).unwrap());
-
-    let mut dataset = Dataset {
-        dim: 0, 
-        num: data_num, 
-        data: HashMap::new(), 
-    };
-
-    let mut buf: [u8; 4] = [0;4];
-    let mut dim: Dim = 0;
-    for id in 0..data_num {
-        let _ = reader.read(&mut buf);
-        dim = Dim::from_le_bytes(buf);
-
-        let mut row = Vec::new();
-        for _j in 0..dim {
-            let _ = reader.read(&mut buf);
-            let val: f32 = f32::from_le_bytes(buf);
-            row.push(val);
-        }
-        dataset.data.insert(id, Rc::new(row));
-    }
-    dataset.dim = dim;
-
-    println!("[Info] -> completed: {:?}", timer.elapsed());
-    
-    dataset
-
+pub trait Load {
+    fn load(&self, filename: &str) -> Result<Dataset, io::Error>;
+    fn partial_load(&self, filename: &str, data_num: u64) -> Result<Dataset, io::Error>;
 }
 
+pub struct Fvecs;
+
+impl Fvecs {
+    fn sequentially_load_fvecs(filename: &str, data_dim: Dim, data_num: u64) -> Result<Dataset, io::Error> {
+        let mut reader = BufReader::new(File::open(filename)?);
+        let mut buf: [u8; 4] = [0;4];
+
+        let mut dataset = Dataset {
+            dim: data_dim, 
+            num: data_num, 
+            data: HashMap::new(), 
+        };
+
+        for id in 0..data_num {
+            let _ = reader.read(&mut buf);
+            let dim = Dim::from_le_bytes(buf);
+            if dataset.dim != dim {
+                panic!("The dimensions of data are not same");
+            }
+
+            let mut row = Vec::new();
+            for _j in 0..(dataset.dim) {
+                let _ = reader.read(&mut buf);
+                let val: f32 = f32::from_le_bytes(buf);
+                row.push(val);
+            }
+            dataset.data.insert(id, Rc::new(row));
+        }
+
+        Ok(dataset)
+
+    }
+
+
+    fn get_data_dim_and_num(valid_filename: &str) -> Result<(Dim, u64), io::Error> {
+        let metadata = fs::metadata(valid_filename)?;
+        let mut reader = BufReader::new(File::open(valid_filename)?);
+
+        let mut buf: [u8; 4] = [0;4];
+
+        let _ = reader.read(&mut buf);
+        let dim = Dim::from_le_bytes(buf);
+        
+        Ok((dim, metadata.len() as u64 / ((dim) as u64 * 4 + 4)))
+
+    }
+}
+
+impl Load for Fvecs {
+    fn load(&self, filename: &str) -> Result<Dataset, io::Error> {
+        //println!("[Info] Load file: {}", filename);
+        let timer = time::Instant::now();
+        let (data_dim, data_num) = match Fvecs::get_data_dim_and_num(filename) {
+            Ok((data_dim, data_num)) => (data_dim, data_num), 
+            Err(error) => {
+                panic!("There was a problem loading the dataset: {:?}", error)
+            }, 
+        };
+        Fvecs::sequentially_load_fvecs(filename, data_dim, data_num)
+        
+    }
+
+    fn partial_load(&self, filename: &str, load_data_num: u64) -> Result<Dataset, io::Error> {
+        //println!("[Info] Load file: {}", filename);
+        let timer = time::Instant::now();
+        let (data_dim, data_num) = match Fvecs::get_data_dim_and_num(filename) {
+            Ok((data_dim, data_num)) => (data_dim, data_num), 
+            Err(error) => {
+                panic!("There was a problem loading the dataset: {:?}", error)
+            }, 
+        };
+        Fvecs::sequentially_load_fvecs(filename, data_dim, std::cmp::min(data_num, load_data_num))
+
+    }
+}
