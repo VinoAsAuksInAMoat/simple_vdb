314ff21d4dda325c3579a9fa1ebd726103ffece1
VinoAsAuksInAMoat <83805270+VinoAsAuksInAMoat@users.noreply.github.com>
2025-08-29 04:48:58 +0900

    refactor: change impl of functions from the struct to the trait

--- src/dataset_manager/dataset_loader.rs
+++ src/dataset_manager/dataset_loader.rs
@@ -13,14 +13,38 @@ use std::{
 use crate::common::data::*;
 
 pub trait Load {
-    fn load(&self, filename: &str) -> Result<Dataset, io::Error>;
-    fn partial_load(&self, filename: &str, data_num: u64) -> Result<Dataset, io::Error>;
+    fn load(&self, filename: &str) -> Result<Dataset, io::Error> {
+        //println!("[Info] Load file: {}", filename);
+        let timer = time::Instant::now();
+        let (data_dim, data_num) = match Self::get_data_dim_and_num(filename) {
+            Ok((data_dim, data_num)) => (data_dim, data_num), 
+            Err(error) => {
+                panic!("There was a problem loading the dataset: {:?}", error)
+            }, 
+        };
+        Self::sequentially_load(filename, data_dim, data_num)
+        
+    }
+    fn partial_load(&self, filename: &str, load_data_num: u64) -> Result<Dataset, io::Error> {
+        //println!("[Info] Load file: {}", filename);
+        let timer = time::Instant::now();
+        let (data_dim, data_num) = match Self::get_data_dim_and_num(filename) {
+            Ok((data_dim, data_num)) => (data_dim, data_num), 
+            Err(error) => {
+                panic!("There was a problem loading the dataset: {:?}", error)
+            }, 
+        };
+        Self::sequentially_load(filename, data_dim, std::cmp::min(data_num, load_data_num))
+
+    }
+    fn sequentially_load(filename: &str, data_dim: Dim, data_num: u64) -> Result<Dataset, io::Error>;
+    fn get_data_dim_and_num(valid_filename: &str) -> Result<(Dim, u64), io::Error>;
 }
 
 pub struct Fvecs;
 
-impl Fvecs {
-    fn sequentially_load_fvecs(filename: &str, data_dim: Dim, data_num: u64) -> Result<Dataset, io::Error> {
+impl Load for Fvecs {
+    fn sequentially_load(filename: &str, data_dim: Dim, data_num: u64) -> Result<Dataset, io::Error> {
         let mut reader = BufReader::new(File::open(filename)?);
         let mut buf: [u8; 4] = [0;4];
 
@@ -61,34 +85,6 @@ impl Fvecs {
         let dim = Dim::from_le_bytes(buf);
         
         Ok((dim, metadata.len() as u64 / ((dim) as u64 * 4 + 4)))
-
-    }
-}
-
-impl Load for Fvecs {
-    fn load(&self, filename: &str) -> Result<Dataset, io::Error> {
-        //println!("[Info] Load file: {}", filename);
-        let timer = time::Instant::now();
-        let (data_dim, data_num) = match Fvecs::get_data_dim_and_num(filename) {
-            Ok((data_dim, data_num)) => (data_dim, data_num), 
-            Err(error) => {
-                panic!("There was a problem loading the dataset: {:?}", error)
-            }, 
-        };
-        Fvecs::sequentially_load_fvecs(filename, data_dim, data_num)
-        
-    }
-
-    fn partial_load(&self, filename: &str, load_data_num: u64) -> Result<Dataset, io::Error> {
-        //println!("[Info] Load file: {}", filename);
-        let timer = time::Instant::now();
-        let (data_dim, data_num) = match Fvecs::get_data_dim_and_num(filename) {
-            Ok((data_dim, data_num)) => (data_dim, data_num), 
-            Err(error) => {
-                panic!("There was a problem loading the dataset: {:?}", error)
-            }, 
-        };
-        Fvecs::sequentially_load_fvecs(filename, data_dim, std::cmp::min(data_num, load_data_num))
 
     }
 }
