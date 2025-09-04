6dbe99f2449203b26d06fd96c013c021ccdc9137
VinoAsAuksInAMoat <83805270+VinoAsAuksInAMoat@users.noreply.github.com>
2025-09-04 22:14:50 +0900

    update: codes of calling new impl of dataset loader in test run (in fn main)

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
-use std::env;
-use std::rc::Rc;
+#[allow(unused_imports)]
+use std::{
+    env, 
+    rc::Rc, 
+    time, 
+};
 
 mod dataset_manager;
 mod common;
 mod search;
 mod evaluation;
+
+use crate::dataset_manager::dataset_loader::*;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -13,15 +19,17 @@ fn main() {
     let data_num: u64 = args[3].parse().unwrap();
     let k_for_search: usize = args[4].parse().unwrap();
 
-    let dataset = dataset_manager::dataset_loader::load_fvecs(&dataset_filename, data_num);
-    println!("[Info] dataset info: dim={}, num={}", dataset.dim, dataset.num);
-    let queries = dataset_manager::dataset_loader::load_fvecs(&query_filename, 1);
-    println!("[Info] queries info: dim={}, num={}", queries.dim, queries.num);
+    let try_num = 1;
+    let loader = Fvecs;
 
-    //let using_index = search::IndexType::BruteForce;
-    //let using_index = search::IndexType::IVFFlat;
-    let using_index = search::IndexType::HNSW;
-    let query = queries.data.get(&0).unwrap();
+    // use load instead of partial_load to load all dataset
+    let dataset = loader.partial_load(&dataset_filename, data_num).unwrap();
+    println!("[Info] dataset info: dim={}, num={}", dataset.dim, dataset.num);
+    let queryset = loader.partial_load(&query_filename, 1).unwrap();
+    println!("[Info] queries info: dim={}, num={}", queryset.dim, queryset.num);
+    
+    let using_index = search::IndexType::IVFFlat; // BruteForce, IVFFlat, HNSW
+    let query = queryset.data.get(&0).unwrap();
     let answers = search::knn_search(using_index, Rc::clone(&query), k_for_search, &dataset);
     evaluation::evaluate_recall(answers.clone(), Rc::clone(&query), k_for_search, &dataset);
 
